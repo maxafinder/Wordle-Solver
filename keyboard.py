9d590feb10163cf1217e354c8a2deb8e05ec3bca
Max Finder <maxafinder@gmail.com>
2022-03-11 01:30:34 -0500

    Create functions that click keys and guess words

--- keyboard.py
+++ keyboard.py
@@ -1,6 +1,8 @@
+from time import sleep
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 # Gets the div containing the keyboard in a selenium WebElement.
@@ -18,3 +20,37 @@ def get_keyboard(driver):
 	# find the <div id="keyboard"> element
 	keyboard = game_keyboard_root.find_element(By.ID, "keyboard")
 	return keyboard
+
+
+# Gets a list of selenium WebElements that are the keyboard keys.
+# @param driver -> the selenium webdriver that opens Wordle
+def get_keys(driver):
+	keyboard = get_keyboard(driver)
+	keys = keyboard.find_elements(By.TAG_NAME, "button")
+	return keys
+
+
+# Clicks the keyboard key with the specified value.
+# @param keys -> list of selenium WebElements that are the keyboard keys.
+# @param val -> the value of the key we want to click.
+def click_key(keys, val):
+	# find the key in keys with val
+	for k in keys:
+		if k.text == val:
+			key = k
+	# click the key
+	key.send_keys(Keys.ENTER)
+
+
+# Guesses the word and waits for board to update.
+# @param keys -> list of selenium WebElements that are the keyboard keys.
+# @param word -> the 5 letter word we want to guess.
+def guess_word(keys, word):
+	if len(word) > 5:
+		print("Invalid guess")
+		return
+	for i in word:
+		click_key(keys, i)
+	click_key(keys, "enter")
+	sleep(2)
+
